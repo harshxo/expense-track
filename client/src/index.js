a63271ae6d0b3229a125f058f388dc3fdce41897
harshxo <shrivastavaharsh876@gmail.com>
2023-09-13 16:00:15 +0530

    route provider

--- client/src/index.js
+++ client/src/index.js
@@ -9,9 +9,9 @@ import store from "./store/index.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
