dd644ff4d7dbd3bfa2e9824f7399be7935e8d617
harshxo <shrivastavaharsh876@gmail.com>
2023-09-15 15:37:11 +0530

    transaction list

---
+++ client/src/components/TransactionList.js
@@ -0,0 +1,62 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+
+
+return (
+    <>
+      <Typography sx={{ marginTop: 10 }} variant="h6">
+        List of Transactions
+      </Typography>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Amount</TableCell>
+              <TableCell align="center">Description</TableCell>
+              <TableCell align="center">Category</TableCell>
+              <TableCell align="center">Date</TableCell>
+              <TableCell align="center">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((month) =>
+              month.transactions.map((row) => (
+                <TableRow
+                  key={row._id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="center" component="th" scope="row">
+                    {row.amount}
+                  </TableCell>
+                  <TableCell align="center">{row.description}</TableCell>
+                  <TableCell align="center">
+                    {categoryName(row.category_id)}
+                  </TableCell>
+                  <TableCell align="center">{formatDate(row.date)}</TableCell>
+                  <TableCell align="center">
+                    <IconButton
+                      color="primary"
+                      component="label"
+                      onClick={() => setEditTransaction(row)}
+                    >
+                      <EditSharpIcon />
+                    </IconButton>
+
+                    <IconButton
+                      color="warning"
+                      component="label"
+                      onClick={() => remove(row._id)}
+                    >
+                      <DeleteSharpIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
